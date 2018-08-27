53be0e8e10e2d41ed1ffc069c3b290a73ba009b5
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-08-27 08:50:41 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- v1App/compPlanilla/departamentos.php
+++ v1App/compPlanilla/departamentos.php
@@ -17,34 +17,30 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Departamentos"] == 1) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-        <link rel="stylesheet" href="../lib/animation/css/animation.css" >
-        <link rel="stylesheet" href="../lib/alertifyjs/css/alertify.css" >
-        <link rel="stylesheet" href="../lib/alertifyjs/css/themes/semantic.min.css" >
-        <link rel="stylesheet" href="css/menusuperior.css" >
-        <link rel="stylesheet" href="css/boxmodel.css" >
-        <link rel="stylesheet" href="css/boxcuerpo.css" >
-        <link rel="stylesheet" href="css/menuseccion.css" >
+           <?php include '../comps/compscss.php'; ?>
         <link rel="stylesheet" href="css/planilla.css" >
-        <link rel="shortcut icon" href="../img/favicon.ico">
-        <link rel="stylesheet" href="css/showinfo.css" >
 
         <title>Hello, world!</title>
     </head>
     <body>
         <a id="iraAncla" href="#ancla"></a>
         <input type="hidden" id="tocompare">
-        <?php include_once './ShowInfo.php'; ?>
-        <?php include './navsuperior.php'; ?>
+       <?php include '../comps/ShowInfo.php'; ?>
+        <?php include '../comps/navsuperior.php'; ?>
 
 
-        <div class="contenedorcentral" id="contenedorcentral">
+        <div>
             <div class="caja1 " id="caja1">
-                <?php include './navlateral.php'; ?>
+                 <?php include '../comps/navlateral.php'; ?>
             </div>
-            <div class="caja2" id="caja2">
-                <div id="cuerpobox" class="container">
+            
+             <div class="caja2" id="caja2">
+                <div class="navbarlateral">
+                    <?php include '../comps/navlateralbar.php'; ?>
+                </div>
+                  <div id="masterbox">
+
+                    <div id="boxcontrol"  class="container-fluid" >
                     <div class="col-lg-12 ">
                         <!-- Cuerpo -->
 
@@ -135,21 +131,18 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Departamentos"] == 1) {
                     </div>
                 </div>
             </div>
+             </div>  
+       
         </div>
 
 
-        <script src="../lib/jquery/jquery-3.2.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-        <script src="../lib/animation/js/animation.js" ></script>
-        <script src="../lib/alertifyjs/js/alertify.js" ></script>
-        <script src="js/validaciones.js" ></script>
-         <script src="js/showinfo.js" ></script>
+          <?php include '../comps/compsjs.php'; ?>
         <script src="controler/insertDepartamentosBox.js" ></script>
         <script src="controler/getDepartamentosBlock.js" ></script>
         <script src="controler/deletedDepartamentos.js" ></script>
         <script src="controler/putInModalDepartamentos.js" ></script>
         <script src="controler/upDateDepartamentos.js" ></script>
+        <script src="js/Rolessize.js" ></script>
        
 
 
