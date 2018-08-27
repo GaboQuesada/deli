4fcd62a2dc214fef95e3b3b14914ed25a6eb275e
Gabrile Quesada Sanchez <gabrielquesadasanchez@gmail.com>
2018-08-27 08:56:29 -0600

    Merge origin/master
    Conflicts:
    	js/usuarios.js

--- v1App/compPlanilla/actividades.php
+++ v1App/compPlanilla/actividades.php
@@ -16,39 +16,31 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Actividades"] == 1) {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+         <?php include '../comps/compscss.php'; ?>
         <link rel="stylesheet" href="css/planilla.css" >
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-        <link rel="stylesheet" href="../lib/animation/css/animation.css" >
-        <link rel="stylesheet" href="../lib/alertifyjs/css/alertify.css" >
-        <link rel="stylesheet" href="../lib/alertifyjs/css/themes/semantic.css" >
-        <link rel="stylesheet" href="css/boxmodel.css" >
-        <link rel="stylesheet" href="css/menusuperior.css" >
-        <link rel="shortcut icon" href="../img/favicon.ico">
-
-        <link rel="stylesheet" href="css/menuseccion.css" >
-        <link rel="stylesheet" href="css/boxcuerpo.css" >
-        <link rel="stylesheet" href="css/showinfo.css" >
+        
 
         <title>Actividades</title>
     </head>
     <body>
-        <?php include_once './ShowInfo.php'; ?>
-        <?php include './navsuperior.php'; ?>
+        <?php include '../comps/ShowInfo.php'; ?>
+        <?php include '../comps/navsuperior.php'; ?>
         <a id="iraAncla" href="#ancla"></a>
         <input type="hidden" id="tocompare">
 
 
 
-        <div class="contenedorcentral" id="contenedorcentral">
+        <div>
             <div class="caja1 " id="caja1">
-              <?php include './navlateral.php'; ?>
+                 <?php include '../comps/navlateral.php'; ?>
             </div>
             <div class="caja2" id="caja2">
-
-                <div id="cuerpobox" class="container">
-
-                    <div class="col-lg-12 ">
+<div class="navbarlateral">
+                    <?php include '../comps/navlateralbar.php'; ?>
+                </div>
+                 <div id="masterbox">
+                     <div id="boxcontrol"  class="container-fluid" >
+                         <div class="col-lg-12 ">
                         <!-- Cuerpo -->
 
 
@@ -142,6 +134,8 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Actividades"] == 1) {
                         <!-- Cuerpo -->                    
 
                     </div>
+                     </div>
+                    
 
                 </div>
 
@@ -149,18 +143,13 @@ if (!empty($_SESSION["usuarioid"]) && $_SESSION["Actividades"] == 1) {
         </div>
 
 
-        <script src="../lib/jquery/jquery-3.2.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-        <script src="../lib/animation/js/animation.js" ></script>
-        <script src="../lib/alertifyjs/js/alertify.js" ></script>
-        <script src="js/validaciones.js"></script>
-        <script src="js/showinfo.js" ></script>
+         <?php include '../comps/compsjs.php'; ?>
         <script src="controler/insertActividadesBox.js" ></script>
         <script src="controler/getActividadesBlock.js" ></script>
         <script src="controler/deletedActividades.js" ></script>
         <script src="controler/putInModalActividades.js" ></script>
         <script src="controler/upDateActividades.js" ></script>
+        <script src="js/Rolessize.js" ></script>
         
 
 
